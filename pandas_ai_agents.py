9a85c4161e1070b766ca8215405477005259ac7d
swarimabdussamad <swarimabdussamad@gmail.com>
2024-12-27 08:55:12 +0530

    remove client variable

--- pandas_ai_agents.py
+++ pandas_ai_agents.py
@@ -15,8 +15,8 @@ if not api_key:
     raise ValueError("GROQ_API_KEY not found in .env file.")
 
 # Initialize the ChatGroq client
-client = ChatGroq(model_name="llama3-70b-8192", api_key=api_key)
-llm = client
+llm = ChatGroq(model_name="llama3-70b-8192", api_key=api_key)
+
 
 # Define the function to detect encoding
 def detect_file_encoding(file_path):
